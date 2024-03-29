cd10e3148e549a8860cb8d900c2ecd8fd4e1fae4
[RajagopalOfficial] <[rajagopal.t@indiumsoft.com]>
2024-03-29 20:32:58 +0530

    edit_commit

--- src/Components/Table/UserTable.jsx
+++ src/Components/Table/UserTable.jsx
@@ -1,9 +1,23 @@
 import React, { useEffect, useState } from "react";
 import MUIDataTable from "mui-datatables";
+import EditIcon from "@mui/icons-material/Edit";
+import CloseIcon from "@mui/icons-material/Close";
+import { IconButton, Modal, TextField } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material";
-
-const UserTable = () => {
+import { Button } from "@mui/material";
+import { Box } from "@mui/material";
+const App = () => {
   const [users, setUsers] = useState([]);
+  const [editModalOpen, setEditModalOpen] = useState(false); //state for modal close or not
+  const [selectedUser, setSelectedUser] = useState(null); //state for maintaining which user is selected for editing
+  const [editedUserData, setEditedUserData] = useState({
+    name: "",
+    title: "",
+    status: "",
+    age: "",
+    role: "",
+  });
+
   const statusOptions = ["ACTIVE", "INACTIVE", "OFFLINE"];
   const roleOptions = ["Admin", "Owner", "Member"];
 
@@ -53,13 +67,13 @@ const UserTable = () => {
       options: {
         customBodyRender: (value, tableMeta, updateValue) => {
           const rowData = users[tableMeta.rowIndex];
-          let statusStyle = {};// An empty Object to  hold the css styles
+          let statusStyle = {}; // An empty Object to  hold the css styles
           let isBold = false;
           switch (rowData.status) {
             case "ACTIVE":
             case "INACTIVE":
             case "OFFLINE":
-              isBold = true;//If any of these 3 cases bold it
+              isBold = true; //If any of these 3 cases bold it
               break;
             default:
               break;
@@ -73,7 +87,7 @@ const UserTable = () => {
           switch (rowData.status) {
             case "ACTIVE":
               statusStyle = {
-                ...statusStyle,//css properties defined in the statusStyles  is retained
+                ...statusStyle, //css properties defined in the statusStyles  is retained
                 color: "green",
                 backgroundColor: "#e6ffe6",
               };
@@ -117,6 +131,19 @@ const UserTable = () => {
       name: "role",
       label: "Role",
     },
+    {
+      name: "edit",
+      label: "Edit User",
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <IconButton onClick={() => handleEditClick(tableMeta.rowIndex)}>
+              <EditIcon />
+            </IconButton>
+          );
+        },
+      },
+    },
   ];
 
   useEffect(() => {
@@ -133,6 +160,87 @@ const UserTable = () => {
         setUsers(local);
       });
   }, []);
+  const handleEditClick = (rowIndex) => {
+    const userData = users[rowIndex];
+    const { id, ...userDataWithoutId } = userData; //destructures and gets the id property and assigns the remaining property to userDataWithoutId
+    setSelectedUser(userData);
+    setEditedUserData({ ...userData });
+    setEditModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setEditModalOpen(false);
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    const nameParts = name.split('.');
+    if (nameParts.length > 1) {
+      const fieldName = nameParts[0];
+      const nestedFieldName = nameParts[1]; 
+      setEditedUserData({
+        ...editedUserData,
+        [fieldName]: {
+          ...editedUserData[fieldName],
+          [nestedFieldName]: value 
+        }
+      });
+    } else {
+      setEditedUserData({
+        ...editedUserData,
+        [name]: value
+      });
+    }
+  };
+  const handleSaveChanges = () => {
+    const updatedUsers = users.map((user) => {
+      if (user.id === editedUserData.id) {
+        let statusStyle = {};
+        let isBold = ["ACTIVE", "INACTIVE", "OFFLINE"].includes(
+          editedUserData.status
+        );
+
+        if (isBold) {
+          statusStyle = {
+            fontWeight: "bold",
+            textAlign: "center",
+          };
+        }
+        switch (editedUserData.status) {
+          case "ACTIVE":
+            statusStyle = {
+              ...statusStyle,
+              color: "green",
+              backgroundColor: "#e6ffe6",
+            };
+            break;
+          case "INACTIVE":
+            statusStyle = {
+              ...statusStyle,
+              color: "brown",
+              backgroundColor: "#ffe0b2",
+            };
+            break;
+          case "OFFLINE":
+            statusStyle = {
+              ...statusStyle,
+              color: "hotpink",
+              backgroundColor: "#ffe6e6",
+            };
+            break;
+          default:
+            break;
+        }
+        return {
+          ...user,
+          ...editedUserData,
+          statusStyle, // Add statusStyle to user object
+        };
+      } else {
+        return user;
+      }
+    });
+    setUsers(updatedUsers);
+    setEditModalOpen(false);
+  };
 
   const options = {
     selectableRows: false,
@@ -157,7 +265,6 @@ const UserTable = () => {
         },
       },
     });
-
   return (
     <div>
       <ThemeProvider theme={getMuiTheme()}>
@@ -168,8 +275,97 @@ const UserTable = () => {
           options={options}
         />
       </ThemeProvider>
+      <Modal
+        open={editModalOpen}
+        onClose={handleModalClose}
+        disableBackdropClick
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width:"40%",
+            maxHeight: "80vh",
+            overflowY: "auto",
+            backgroundColor: "#fff",
+            padding: "20px",
+            borderRadius: "8px",
+            boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <IconButton
+            style={{ position: "absolute", top: "10px", right: "10px" }}
+            onClick={handleModalClose}
+          >
+            <CloseIcon />
+          </IconButton>
+
+          <h2>Edit User</h2>
+          <form>
+            <Box mb={2}>
+              <TextField
+                name="email"
+                label="Email"
+                value={editedUserData.email}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+            </Box>
+            <Box mb={2}>
+              <TextField
+                name="company.title"
+                label="Title"
+                value={editedUserData.company?.title}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+            </Box>
+            <Box mb={2}>
+              <TextField
+                name="status"
+                label="Status"
+                value={editedUserData.status}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+            </Box>
+            <Box mb={2}>
+              <TextField
+                name="age"
+                label="Age"
+                value={editedUserData.age}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+            </Box>
+            <Box mb={2}>
+              <TextField
+                name="role"
+                label="Role"
+                value={editedUserData.role}
+                onChange={handleInputChange}
+                fullWidth
+                margin="normal"
+              />
+            </Box>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSaveChanges}
+            >
+              Save Changes
+            </Button>
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 };
 
-export default UserTable;
+export default App;
