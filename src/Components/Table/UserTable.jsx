f7fda1dae606b94a589d7253dc6a17e49904f0a0
[RajagopalOfficial] <[rajagopal.t@indiumsoft.com]>
2024-04-01 14:53:01 +0530

    Delete_Commit

--- src/Components/Table/UserTable.jsx
+++ src/Components/Table/UserTable.jsx
@@ -4,30 +4,40 @@ import EditIcon from "@mui/icons-material/Edit";
 import CloseIcon from "@mui/icons-material/Close";
 import { IconButton, Modal, TextField } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
-import AddCircleIcon from '@mui/icons-material/AddCircle'
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
 const App = () => {
-  const [users, setUsers] = useState([]);//State for populating the users
+  const [users, setUsers] = useState([]); //State for populating the users
   const [editModalOpen, setEditModalOpen] = useState(false); //state for modal close or not
   const [selectedUser, setSelectedUser] = useState(null); //state for maintaining which user is selected for editing
-  const [editedUserData, setEditedUserData] = useState({//state for maintaining data in edit section
+  const [editedUserData, setEditedUserData] = useState({
+    //state for maintaining data in edit section
     name: "",
     title: "",
     status: "",
     age: "",
     role: "",
-    image:""
+    image: "",
   });
-  const [editedName, setEditedName] = useState("");// state for maintaining the name property alone in edit section
-
+  const [editedName, setEditedName] = useState(""); // state for maintaining the name property alone in edit section
+  const [deleteDialogOpen, setDeleteDialogOpen] = useState(false); // State for controlling the delete confirmation dialog
+  const [deleteRowIndex, setDeleteRowIndex] = useState(null); // State for storing the index of the row to be deleted
   const statusOptions = ["ACTIVE", "INACTIVE", "OFFLINE"];
   const roleOptions = ["Admin", "Owner", "Member"];
 
   const columns = [
     {
-      name:'id',
-      label:'Id'
+      name: "id",
+      label: "Id",
     },
     {
       name: "name",
@@ -36,28 +46,34 @@ const App = () => {
         customBodyRender: (value, tableMeta, updateValue) => {
           const rowData = users[tableMeta.rowIndex];
           return (
-            <div style={{ display: "flex", alignItems: "center" ,position:"relative"}}>
-              <div style={{position:'relative'}}>
-              <img
-                src={rowData.image}
-                alt="Profile"
-                style={{
-                  width: 50,
-                  height: 50,
-                  marginRight: 10,
-                  borderRadius: "50%"
-                }}
-              />
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                position: "relative",
+              }}
+            >
+              <div style={{ position: "relative" }}>
+                <img
+                  src={rowData.image}
+                  alt="Profile"
+                  style={{
+                    width: 50,
+                    height: 50,
+                    marginRight: 10,
+                    borderRadius: "50%",
+                  }}
+                />
                 <AddCircleIcon
-                style={{
-                  position: "absolute",
-                  top: -5,
-                  right: -5,
-                  color: "black",
-                  cursor: "pointer",
-                }}
-                onClick={() => handleImageClick(rowData.id)}
-              />
+                  style={{
+                    position: "absolute",
+                    top: -5,
+                    right: -5,
+                    color: "black",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleImageClick(rowData.id)}
+                />
               </div>
               <div>
                 <div>
@@ -156,8 +172,32 @@ const App = () => {
       options: {
         customBodyRender: (value, tableMeta, updateValue) => {
           return (
-            <IconButton onClick={() => handleEditClick(tableMeta.rowIndex)}>
+            <IconButton
+              onClick={() => handleEditClick(tableMeta.rowIndex)}
+              style={{
+                color: tableMeta.rowIndex === selectedUser ? "blue" : "inherit",
+              }}
+            >
               <EditIcon />
+            </IconButton>
+          );
+        },
+      },
+    },
+    {
+      name: "delete",
+      label: "Delete User",
+      options: {
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <IconButton
+              onClick={() => handleDeleteClick(tableMeta.rowIndex)}
+              style={{
+                color:
+                  tableMeta.rowIndex === deleteRowIndex ? "red" : "inherit",
+              }}
+            >
+              <DeleteIcon />
             </IconButton>
           );
         },
@@ -181,9 +221,9 @@ const App = () => {
   }, []);
   const handleEditClick = (rowIndex) => {
     const userData = users[rowIndex];
-    setSelectedUser(userData);
-    setEditedUserData({ ...userData});
-    setEditedName(`${userData.firstName} ${userData.lastName}`)
+    setSelectedUser(rowIndex);
+    setEditedUserData({ ...userData });
+    setEditedName(`${userData.firstName} ${userData.lastName}`);
     setEditModalOpen(true);
   };
   const handleModalClose = () => {
@@ -192,32 +232,34 @@ const App = () => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
 
-    const nameParts = name.split('.');//for nested properties example company.title
+    const nameParts = name.split("."); //for nested properties example company.title
     if (nameParts.length > 1) {
       const fieldName = nameParts[0];
-      const nestedFieldName = nameParts[1]; 
+      const nestedFieldName = nameParts[1];
       setEditedUserData({
         ...editedUserData,
         [fieldName]: {
-          ...editedUserData[fieldName],//for fieldname property in editeduserData keep the Outer object as it is and change the inner nested object's property alone
-          [nestedFieldName]: value 
-        }
+          ...editedUserData[fieldName], //for fieldname property in editeduserData keep the Outer object as it is and change the inner nested object's property alone
+          [nestedFieldName]: value,
+        },
       });
     } else {
       setEditedUserData({
         ...editedUserData,
-        [name]: value
+        [name]: value,
       });
     }
   };
   const handleImageClick = (userId) => {
     const input = document.createElement("input");
-    input.type = "file";//sets the input type as file
-    input.accept = "image/*";//accepts images only
-    input.addEventListener("change", (event) => {//change event is triggered ,when user selects a file in input field
-      const file = event.target.files[0];//retrives first file selected by the user
+    input.type = "file"; //sets the input type as file
+    input.accept = "image/*"; //accepts images only
+    input.addEventListener("change", (event) => {
+      //change event is triggered ,when user selects a file in input field
+      const file = event.target.files[0]; //retrives first file selected by the user
       const reader = new FileReader();
-      reader.onload = () => {//will execute when the selected image is completely loaded
+      reader.onload = () => {
+        //will execute when the selected image is completely loaded
         const updatedUsers = users.map((user) => {
           if (user.id === userId) {
             return { ...user, image: reader.result }; // Update the user's image with the selected file
@@ -230,12 +272,32 @@ const App = () => {
     });
     input.click();
   };
+  const handleDeleteClick = (rowIndex) => {
+    setDeleteRowIndex(rowIndex); // Store the index of the row to be deleted
+    setDeleteDialogOpen(true); // Open the delete confirmation dialog
+  };
+  const handleDeleteConfirmed = () => {
+    const userData = users[deleteRowIndex];
+    const updatedUsers = users.filter((_, index) => index !== deleteRowIndex);
+    const updatedUsersWithIds = updatedUsers.map((user, index) => ({
+      ...user,
+      id: index + 1,
+    }));
+    setUsers(updatedUsersWithIds);
+    setDeleteRowIndex(null);
+    setDeleteDialogOpen(false);
+  };
+  const handleDeleteCancelled = () => {
+    setDeleteRowIndex(null); // Reset the stored index of the row to be deleted
+    setDeleteDialogOpen(false); // Close the delete confirmation dialog
+  };
   const handleSaveChanges = () => {
     const updatedUsers = users.map((user) => {
       if (user.id === editedUserData.id) {
         const nameParts = editedName.split(" ");
         const firstName = nameParts[0];
-        const lastName = nameParts.length > 1 ? nameParts.slice(1).join(" ") : "";//joins the whole name
+        const lastName =
+          nameParts.length > 1 ? nameParts.slice(1).join(" ") : ""; //joins the whole name
         let statusStyle = {};
         let isBold = ["ACTIVE", "INACTIVE", "OFFLINE"].includes(
           editedUserData.status
@@ -288,6 +350,7 @@ const App = () => {
     });
     setUsers(updatedUsers);
     setEditModalOpen(false);
+    setSelectedUser(null);
   };
 
   const options = {
@@ -334,7 +397,7 @@ const App = () => {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width:"40%",
+            width: "40%",
             maxHeight: "80vh",
             overflowY: "auto",
             backgroundColor: "#fff",
@@ -352,12 +415,12 @@ const App = () => {
 
           <h2>Edit User</h2>
           <form>
-          <Box mb={2}>
+            <Box mb={2}>
               <TextField
                 name="name"
                 label="Name"
                 value={editedName}
-                onChange={(e)=>setEditedName(e.target.value)}
+                onChange={(e) => setEditedName(e.target.value)}
                 fullWidth
                 margin="normal"
               />
@@ -422,6 +485,22 @@ const App = () => {
           </form>
         </div>
       </Modal>
+      <Dialog open={deleteDialogOpen} onClose={handleDeleteCancelled}>
+        <DialogTitle id="alert-dialog-title">{"Delete User"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this user?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDeleteCancelled} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleDeleteConfirmed} color="primary" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
