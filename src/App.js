4887dd5b6ac54b1e4af258cdfcbd3ab54c047a29
[RajagopalOfficial] <[rajagopal.t@indiumsoft.com]>
2024-04-03 17:08:47 +0530

    Task_04_Commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NestedMenuItem, { NestedMenu } from './Components/NestedMenu/NestedMenu'
 import UserTable from './Components/Table/UserTable';
 import Echarts from './Components/Charts/Echarts'
 import FormValidation from './Components/FormValidation/FormValidation'
+import DisplayTable from './Components/DisplayTable/DisplayTable'
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
@@ -26,6 +27,7 @@ function App() {
       <Button sx={{marginX:"10px"}} variant='contained' onClick={() => handleButtonClick("UserTable")}>Open User Table with Edit and Delete Functionality</Button>
       <Button sx={{marginX:"10px"}} variant='contained' onClick={() => handleButtonClick("Echarts")}>Open a Sample Echart</Button>
       <Button sx={{marginX:"10px"}} variant='contained' onClick={() => handleButtonClick("FormValidation")}>FormValidation</Button>
+      <Button sx={{marginX:"10px"}} variant='contained' onClick={() => handleButtonClick("DisplayTable")}>DisplayTable</Button>
       <Modal open={openModal} onClose={handleCloseModal} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <div style={{ position: 'relative', width: '1600px', backgroundColor: 'white', padding: '20px' }}>
           <IconButton style={{ position: 'absolute', top: '10px', right: '10px', zIndex: 1 }} onClick={handleCloseModal}>
@@ -35,7 +37,7 @@ function App() {
           {selectedComponent === "UserTable" && <UserTable />}
           {selectedComponent === "Echarts" && <Echarts />}
           {selectedComponent === "FormValidation" && <FormValidation />}
-      
+          {selectedComponent==="DisplayTable"&&<DisplayTable />}
         </div>
       </Modal>
     </div>
